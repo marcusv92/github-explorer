d678211059b1f8c15db510a209b054fc099d4f8f
marcusv92 <47533367+marcusv92@users.noreply.github.com>
2023-04-03 21:50:48 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard'; // Importa o componente Dashboard
 import Repository from '../pages/Repository'; // Importa o componente Repository
 
+// Define as rotas da aplicação
 const Routes: React.FC = () => (
   <Switch>
-    // Define a rota da página inicial da aplicação
+    {/* Define a rota da página inicial da aplicação */}
     <Route path="/" exact component={Dashboard} />
-    // Define a rota da página de um repositório específico
+    {/* Define a rota da página de um repositório específico */}
     <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
