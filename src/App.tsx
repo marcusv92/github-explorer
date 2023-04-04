230dbb852eefa122e9371b744dc537ba21a1a666
marcusv92 <47533367+marcusv92@users.noreply.github.com>
2023-04-03 21:56:30 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'; // Importa a biblioteca React
+import { BrowserRouter } from 'react-router-dom'; // Importa o componente BrowserRouter da biblioteca react-router-dom
+import GlobalStyle from './styles/global'; // Importa o componente de estilos globais
+import Routes from './routes'; // Importa o componente de rotas da aplicação
 
-import GlobalStyle from './styles/global';
-import Routes from './routes';
-
+// Define o componente principal da aplicação
 const App: React.FC = () => (
   <>
+    {/* Define o componente de roteamento da aplicação */}
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
+    {/* Define o componente de estilos globais */}
     <GlobalStyle />
   </>
 );
 
+// Exporta o componente principal da aplicação
 export default App;
