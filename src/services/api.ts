a53770eff29674e66c1350075a876fe12c8021a3
marcusv92 <47533367+marcusv92@users.noreply.github.com>
2023-04-03 21:53:25 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from 'axios'; // Importa a biblioteca axios
 
+// Cria uma instância da biblioteca axios com a baseURL da API do GitHub
 const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
+// Exporta a instância da API criada
 export default api;
